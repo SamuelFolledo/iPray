625c2bc4b78c97386e0e2752f0f49a7d072b7609
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-16 10:41:51 -0700

    [add] mp3 songs downloaded from Youtube via ytmp3.cc initialized songtableview cell

---
+++ Folledo-iPray/Views/SongCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongCell.swift
+//  Folledo-iPray
+//
+//  Created by Macbook Pro 15 on 10/16/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class SongCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
