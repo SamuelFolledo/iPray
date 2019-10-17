573a8dcb612d02700151921a2ed3659647061efc
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-17 11:23:53 -0700

    [add] Service methods for presenting alert and popping back to previous controllers

--- Folledo-iPray/Helpers/Service.swift
+++ Folledo-iPray/Helpers/Service.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2019 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Service {
+    func popBack(on: UIViewController, nb: Int) { //method that pops View controller to a certain amount nb
+        if let viewControllers: [UIViewController] = on.navigationController?.viewControllers {
+            guard viewControllers.count < nb else {
+                on.navigationController?.popToViewController(viewControllers[viewControllers.count - nb], animated: true)
+                return
+            }
+        }
+    }
+    
+//presentAlert
+    static func presentAlert(on: UIViewController, title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            alertVC.dismiss(animated: true, completion: nil)
+        }
+        alertVC.addAction(okAction)
+        on.present(alertVC, animated: true, completion: nil)
+    }
+}
